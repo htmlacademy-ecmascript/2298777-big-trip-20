8275f05dc0cafd5e6899f2e1ab47a62e1f47edb2
Yuriy <originalgamerdaddy@icloud.com>
2023-04-28 00:21:08 +0500

    delete render from main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-import { render } from './render';
-import FilterView from './view/filter-view';
 import ListPresenter from './presenter/list-presenter';
 import HeaderPresenter from './presenter/header-presenter';
 
 const tripEvents = document.querySelector('.trip-events');
 const tripMain = document.querySelector('.trip-main');
+const filterContainer = document.querySelector('.trip-controls__filters');
 
 const listPresenter = new ListPresenter(tripEvents);
-const headerPresenter = new HeaderPresenter(tripMain);
+const headerPresenter = new HeaderPresenter(tripMain, filterContainer);
 
-render(new FilterView(), document.querySelector('.trip-controls__filters'));
-listPresenter.init();
 headerPresenter.init();
+listPresenter.init();
