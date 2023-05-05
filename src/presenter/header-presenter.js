64a00567360c20e3e2acf99bfdfadba1e7be1558
Yuriy <originalgamerdaddy@icloud.com>
2023-05-05 14:17:11 +0500

    2.

--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -2,6 +2,8 @@ import { render, RenderPosition } from '../framework/render';
 import TripInfoView from '../view/trip-main-info-view';
 import FilterView from '../view/filter-view';
 import { filter } from '../util/filters';
+import { humanizeDate } from '../util/utils';
+import { DateFormats } from '../consts';
 
 export default class HeaderPresenter {
   #headerContainer;
@@ -17,7 +19,7 @@ export default class HeaderPresenter {
   init() {
     render(new FilterView(this.#generateFilters()), this.#filterContainer);
     if (this.#pointModel.getPoints().length !== 0) {
-      render(new TripInfoView(), this.#headerContainer, RenderPosition.AFTERBEGIN);
+      render(new TripInfoView(this.#generateMainInfo()), this.#headerContainer, RenderPosition.AFTERBEGIN);
     }
   }
 
@@ -26,5 +28,34 @@ export default class HeaderPresenter {
       name: filterName,
       filter: filterFunction(this.#pointModel.getPoints()),
     }));
+  }
+
+  #generateMainInfo() {
+    const createTitle = () => {
+      const destinations = this.#pointModel.getDestinationsInfo();
+
+      const titleArray = [];
+      destinations
+        .map((item) => {
+          if (!titleArray.includes(item.name) ||
+            (titleArray.length !== 0 && item.name !== titleArray[titleArray.length - 1])) {
+            titleArray.push(item.name);
+          }
+        });
+      return titleArray.join(' — ');
+    };
+
+    const createDateFromTo = () => {
+      const points = this.#pointModel.getPoints();
+      const dateFrom = points[0].dateFrom;
+      const dateTo = points[points.length - 1].dateTo;
+      return `${humanizeDate(dateFrom, DateFormats.MONTH_WITH_DAY)} — ${humanizeDate(dateTo, DateFormats.MONTH_WITH_DAY)}`;
+    };
+
+    return {
+      title: createTitle(),
+      dateFromTo: createDateFromTo(),
+      price: this.#pointModel.getPoints().reduce((acc, point) => acc + point.basePrice, 0),
+    };
   }
 }
--- src/view/trip-main-info-view.js
+++ src/view/trip-main-info-view.js
@@ -1,19 +1,26 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const createTripInfoTemplate = () => `<section class="trip-main__trip-info  trip-info">
+const createTripInfoTemplate = (mainInfo) => `<section class="trip-main__trip-info  trip-info">
 <div class="trip-info__main">
-  <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
+  <h1 class="trip-info__title">${mainInfo.title}</h1>
 
-  <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;20</p>
+  <p class="trip-info__dates">${mainInfo.dateFromTo}</p>
 </div>
 
 <p class="trip-info__cost">
-  Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
+  Total: €&nbsp;<span class="trip-info__cost-value">${mainInfo.price}</span>
 </p>
 </section>`;
 
 export default class TripInfoView extends AbstractView{
+  #mainInfo;
+
+  constructor(mainInfo) {
+    super();
+    this.#mainInfo = mainInfo;
+  }
+
   get template() {
-    return createTripInfoTemplate();
+    return createTripInfoTemplate(this.#mainInfo);
   }
 }
